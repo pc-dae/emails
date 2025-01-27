904187a1874a904eea3e940294652651514e0f01
Paul Carlton <paul.carlton414@gmail.com>
2025-01-27 18:48:25 +0000

    tester updates

--- src/main/java/mn/dae/pc/emails/service/EmailService.java
+++ src/main/java/mn/dae/pc/emails/service/EmailService.java
@@ -1,16 +1,29 @@
 package mn.dae.pc.emails.service;
 
+import mn.dae.pc.emails.MainApplication;
 import mn.dae.pc.emails.model.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.io.PrintWriter;
 
 @Service
 public class EmailService {
 
-    public void send(Email email) {
+    private static final Logger log = LoggerFactory.getLogger(MainApplication.class);
 
+    public void send(Email email) {
+        try {
+            PrintWriter out = new PrintWriter( String.format("%s/test.html", System.getProperty("user.home")));
+            out.println(email.getData().get("data"));
+            out.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
